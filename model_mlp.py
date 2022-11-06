4b89670fab99287dc132c4f71e977e984467a8a0
Tiago Onofre <taraujonofre@gmail.com>
2022-11-05 23:38:28 -0300

    feat: find out best function and number of neurons

--- model_mlp.py
+++ model_mlp.py
@@ -1,5 +1,6 @@
 from numpy import mean
 from numpy import std
+from statistics import median
 from sklearn import metrics
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import KFold # Import kfold function
@@ -25,6 +26,7 @@ bases = ['base_16_hog', 'base_16_pca', 'base_16_fs', 'base_20_hog']
 training_test = ['10-fold CV', '70/30', '80/20', '90/10']
 activation_funcs = ['identity', 'logistic', 'tanh', 'relu']
 
+tests_results = []
 
 ## Auxiliar Functions
 def for_each_config(model, X, Y, config_description):
@@ -51,48 +53,104 @@ def for_each_config(model, X, Y, config_description):
                                                                     random_state=1)
     # 10 fold cv
     scores = cross_val_score(model, X, Y.ravel(), scoring='accuracy', cv=kf)
-    print(f'10-fold {config_description} CV Accuracy: {mean(scores)} ({std(scores)})')
+    aux_str = f'10-fold {config_description} Accuracy: {mean(scores)} ({std(scores)})'
+    tests_results.append(aux_str)
+    print(aux_str)
 
     # 70/30
     model.fit(X_train_70, Y_train_70.ravel())
     Y_pred_70_30 = model.predict(X_test_30)
     accuracy_70_30 = metrics.accuracy_score(Y_test_30, Y_pred_70_30)
-    print(f'70/30 {config_description} Accuracy: {accuracy_70_30}')
+    aux_str = f'70/30 {config_description} Accuracy: {accuracy_70_30}'
+    tests_results.append(aux_str)
+    print(aux_str)
 
     # 80/20
     model.fit(X_train_80, Y_train_80.ravel())
     Y_pred_80_20 = model.predict(X_test_20)
     accuracy_80_20 = metrics.accuracy_score(Y_test_20, Y_pred_80_20)
-    print(f'80/20 {config_description} Accuracy: {accuracy_80_20}')
+    aux_str = f'80/20 {config_description} Accuracy: {accuracy_80_20}'
+    tests_results.append(aux_str)
+    print(aux_str)
 
     # 90/10
     model.fit(X_train_90, Y_train_90.ravel())
     Y_pred_90_10 = model.predict(X_test_10)
     accuracy_90_10 = metrics.accuracy_score(Y_test_10, Y_pred_90_10)
-    print(f'90/10 {config_description} Accuracy: {accuracy_90_10}')
+    aux_str = f'90/10 {config_description} Accuracy: {accuracy_90_10}'
+    tests_results.append(aux_str)
+    print(aux_str)
 
-# RUNS
+# TEST RUNS
 # 1
-def fixed_A():
+def const_A():
     '''Running MLP with A = (number of attributes + classes) / 2'''
     print('>>> Running MLP with A = (number of attributes + classes) / 2')
 
-    for ds, base in zip(datasets, bases):
+    ds = datasets[0]
 
-        num_attr = len(ds.columns) - 1
+    A = int(((len(ds.columns) - 1) + 5)/2)
 
-        # Features and classes
-        features = ds.columns.drop('class')
-        X = ds.loc[:, features].values
-        Y = ds.loc[:, ['class']].values
+    # Features and classes
+    features = ds.columns.drop('class')
+    X = ds.loc[:, features].values
+    Y = ds.loc[:, ['class']].values
 
-        for func in activation_funcs:
-            print(f'>>>>>> BASE: {base} | FUNC : {func}')
-            mlp = MLPClassifier(hidden_layer_sizes=num_attr,
-                                activation=func,
-                                solver='adam')
+    for func in activation_funcs:
+        print(f'>>>>>> BASE: {bases[0]} | FUNC: {func} | NEURONS: {A}')
+        mlp = MLPClassifier(hidden_layer_sizes=A,
+                            activation=func,
+                            solver='adam',
+                            max_iter=500)
 
-            for_each_config(mlp, X, Y, 'const A')
+        for_each_config(mlp, X, Y, f'const A {func}')
+
+
+    for result in tests_results:
+            print(result)
+
+# 2
+def different_neurons():
+    '''Running MLP with different number of neurons:
+    A  = attr + class / 2
+    O  = class
+    T  = attr
+    AO = Median value between A and O
+    AT = Median value between A and T
+
+    this one requires to plot a graph where:
+    axis X is the number of neurons and axis Y is the number of accuracy'''
+    print('>>> Running MLP with different number of neurons')
+
+    ds = datasets[0]
+
+    A  = int(((len(ds.columns) - 1) + 5)/2)
+    O  = 5
+    T  = len(ds.columns) - 1
+    AO = int(median([A, O]))
+    AT = int(median([A, T]))
+
+    neurons = [A, O, T, AO, AT]
+
+    # Features and classes
+    features = ds.columns.drop('class')
+    X = ds.loc[:, features].values
+    Y = ds.loc[:, ['class']].values
+
+    for neuron in neurons:
+        print(f'>>>>>> BASE: {bases[0]} | FUNC: relu | NEURONS: {neuron}')
+        mlp = MLPClassifier(hidden_layer_sizes=neuron,
+                            activation='relu',
+                            solver='adam',
+                            max_iter=500)
+
+        for_each_config(mlp, X, Y, f'neurons {neuron}')
+
+
+    for result in tests_results:
+            print(result)
+
+
 
 
 #print('>>> Running MLP with n configurations on each dataset')
@@ -200,4 +258,10 @@ def fixed_A():
 #print('>>> Writing Excel')
 #df.to_excel('./sheets/mlp_results.xlsx', sheet_name='Multilayer Perceptron Results')
 
-fixed_A()
+# DONE const A to discover the best activation func to my base
+# const_A()
+# Best function to my base: relu
+
+# DONE const activation func to discover the best number of neurons
+# different_neurons()
+# Best number of neurons: 884
